f0be6b60cbc241945568b3577e91b03b88da6f98
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2022-01-12 05:30:26 +0330

    fix: add lang and dir to document html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,19 +3,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="fa" dir="rtl">
         <Head>
           <link
+            rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
-            rel="stylesheet"
           />
           <link
+            rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&display=swap"
-            rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap"
             rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap"
           />
         </Head>
 
