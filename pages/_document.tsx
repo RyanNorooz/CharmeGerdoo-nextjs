9395873b46e46dd787c40b6e0fe23ad4a5f9c2a2
Ryan Norooz <58340667+RyanNorooz@users.noreply.github.com>
2022-01-13 18:54:07 +0330

    perf: improve google font loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,17 +5,21 @@ export default class MyDocument extends Document {
     return (
       <Html lang="fa" dir="rtl">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
           />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&display=swap"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400&display=swap"
+            href={
+              'https://fonts.googleapis.com/css2?' +
+              'family=Abril+Fatface&' +
+              'family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&' +
+              'family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&' +
+              'display=swap'
+            }
           />
         </Head>
 
